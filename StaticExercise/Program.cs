575dbef4cd30e5af6e29b3a77257ea96593922ec
tyson lind <tysonfloydlind@gmail.com>
2024-01-20 19:27:32 -0700

    completed static exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            double tempInC = TempConverter.FahrenheitToCelcius(25);
+            Console.WriteLine(tempInC.ToString());
+            double tempInF = TempConverter.CelciusToFahrenheit(-3.88889);
+            Console.WriteLine(tempInF.ToString());
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public class TempConverter
+    {
+        public static double FahrenheitToCelcius (double temp)
+        {
+            return (temp - 32) * 5/9;
+        }
+        public static double CelciusToFahrenheit (double temp)
+        {
+            return (temp * 9 / 5) + 32;
+        }
+    }
+}
